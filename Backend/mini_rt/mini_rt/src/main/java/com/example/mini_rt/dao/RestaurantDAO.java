d1e7c1cf0dedbef98e455382cc548aeb9510ea03
thkim1230 <thkim1347@gmail.com>
2023-04-25 20:44:11 +0900

    4/25

--- Backend/mini_rt/mini_rt/src/main/java/com/example/mini_rt/dao/RestaurantDAO.java
+++ Backend/mini_rt/mini_rt/src/main/java/com/example/mini_rt/dao/RestaurantDAO.java
@@ -50,25 +50,18 @@ public class RestaurantDAO {
     public List<RestaurantInfoVO> RtSelect(RestaurantInfoVO restaurantInfoVO){
         List<RestaurantInfoVO> list = new ArrayList<>();
         try{
-            String sql = "SELECT RESTAURANT_NOTICE,RESTAURANT_PHONE,RESTAURANT_INTRODUCE,RESTAURANT_HOURS,RESTAURANT_ADDR FROM RESTAURANT_INFO WHERE RESTAURANT_ID = ?";
+            String sql = "SELECT RESTAURANT_NAME, RESTAURANT_PHONE, RESTAURANT_ADDR, AVG(RATING) FROM RESTAURANT JOIN RESTAURANT_INFO ON RESTAURANT.RESTAURANT_ID = RESTAURANT_INFO.RESTAURANT_ID JOIN REVIEW ON RESTAURANT.RESTAURANT_ID = REVIEW.RESTAURANT_ID WHERE RESTAURANT.RESTAURANT_ID = ? GROUP BY RESTAURANT.RESTAURANT_NAME, RESTAURANT.RESTAURANT_PHONE, RESTAURANT.RESTAURANT_ADDR";
             conn = Common.getConnection();
             pStmt = conn.prepareStatement(sql);
             pStmt.setString(1,restaurantInfoVO.getRestaurantId());
             rs = pStmt.executeQuery();
             while (rs.next()){
-                String notice = rs.getString("RESTAURANT_NOTICE");
+                String name = rs.getString("RESTAURANT_NAME");
                 String phone = rs.getString("RESTAURANT_PHONE");
-                String introduce = rs.getString("RESTAURANT_INTRODUCE");
-                String hours = rs.getString("RESTAURANT_HOURS");
                 String addr = rs.getString("RESTAURANT_ADDR");
+                String rating = rs.getString("AVG(RATING)");
 
-                RestaurantInfoVO vo = new RestaurantInfoVO();
-                vo.setRestaurantNotice(notice);
-                vo.setRestaurantPhone(phone);
-                vo.setRestaurantIntroduce(introduce);
-                vo.setRestaurantHours(hours);
-                vo.setRestaurantAddr(addr);
-                list.add(vo);
+
             }
             Common.close(rs);
             Common.close(pStmt);
